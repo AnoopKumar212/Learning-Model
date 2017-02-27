af9e3e73e7aa2bab577efb4309229f64af420100
root <root@localhost.localdomain>
2017-02-27 21:10:55 +0530

    login title changes

--- Signup_and_Login/signup_login_form.php
+++ Signup_and_Login/signup_login_form.php
@@ -2,7 +2,7 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>Let's Go</title>  
+  <title>Learning Model</title>  
       <link rel="stylesheet" href="Assets/Style/go.css">
 	  <script src="Assets/Script/Validate.js"></script>	  	  
 </head>
