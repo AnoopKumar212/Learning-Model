36dbfe30106e5355cf197dc5a4b02bff396f6643
cyborgnoah <guptapranav360@gmail.com>
2017-05-10 20:50:36 -0700

    Correct Anchor Tab
    Link to Registered Users Corrected

--- index.php
+++ index.php
@@ -14,25 +14,25 @@
 	<link rel="stylesheet" href="./Assets/Style/w3.css">
 	<link rel="stylesheet" href="./Assets/Style/raleway.css">
 	<link rel="stylesheet" href="./Assets/Style/index_custom.css">
-	
+
 	<body>
 		<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-			
+
 			<div class="w3-display-topleft w3-padding-large w3-xlarge">
 				Learning Model
 			</div>
-		
+
 			<div class="w3-display-middle">
 				<h1 class="w3-jumbo w3-animate-top">Learning Model</h1>
 				<hr class="w3-border-grey" style="margin:auto;width:40%">
-				<p class="w3-large w3-center"><button class="button button_signup" id="signup" onclick="window.location.href='Signup_and_Login/signup_login_form.php'"><b>Let's Start >></b></button></p>
-				<p class="w3-large w3-center"><button class="button button_signup" id="signup" onclick="window.location.href='../Registered_Users_List/registered_users_list.php'"><b>Registered Users >>></b></button></p>
-			</div>  
-		
+				<p class="w3-large w3-center"><button class="button button_signup" id="signup" onclick="window.location.href='./Signup_and_Login/signup_login_form.php'"><b>Let's Start >></b></button></p>
+				<p class="w3-large w3-center"><button class="button button_signup" id="signup" onclick="window.location.href='./Registered_Users_List/registered_users_list.php'"><b>Registered Users >>></b></button></p>
+			</div>
+
 			<div class="w3-display-bottomleft w3-padding-large">
 				Powered by <a href="" target="">Tata Magic</a>
 			</div>
-		
+
 		</div>
 	</body>
 </html>
